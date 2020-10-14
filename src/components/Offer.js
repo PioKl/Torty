394a905bf7eb2181035606c7439cd6701e0999d9
PioKl <kosiu342@gmail.com>
2020-10-14 21:42:48 +0200

    Offer.js|New Offer component

---
+++ src/components/Offer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style/Offer.scss';
+const Offer = () => {
+    return ( 
+        <div className="offer">
+            <h1>Oferta</h1>
+        </div>
+     );
+}
+ 
+export default Offer;
