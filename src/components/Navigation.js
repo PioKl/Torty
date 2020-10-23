dae4ebde74beeb83cdd29058af00baa3759d0e17
PioKl <kosiu342@gmail.com>
2020-10-23 18:25:41 +0200

    Navigation.js|Add IsDesktopContext

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import '../style/Navigation.scss';
+import { IsDesktopContext } from '../contexts/IsDesktopContext';
 
 import { ReactComponent as MenuIcon } from '../svg/menuIcon.svg';
 import { ReactComponent as CakeLogo } from '../svg/cakeLogo.svg';
 const Navigation = () => {
+    const { width } = useContext(IsDesktopContext);
     return ( 
         <div className="navigation">
             <CakeLogo className="navigation__logo"/>
+            {width >= 1280 ?
+                <div className="navigation__menu-desktop">
+                    <ul className="desktop-itemList">
+                        <li className="desktop-itemList__item">Strona główna</li>
+                        <li className="desktop-itemList__item">Oferta</li>
+                        <li className="desktop-itemList__item">Galeria</li>
+                        <li className="desktop-itemList__item">Kontakt</li>
+                    </ul>
+                </div>
+            :
             <MenuIcon className="navigation__menu"/>
+            }
         </div>
      );
 }
