913c25df8e5c49637071be8c795d042987bbc4a2
PioKl <kosiu342@gmail.com>
2020-11-04 18:20:41 +0100

    Offer.js|Add new classes

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../style/Offer.scss';
 import kucharz from "../images/kucharzMobile.png";
+/* import { ReactComponent as OfferWave } from '../svg/offerWave.svg'; */
+import offerWave from "../images/offerWave.png";
+
 const Offer = () => {
     return ( 
         <div className="offer">
@@ -14,24 +17,31 @@ const Offer = () => {
                 <div className="offer__right-side">
                     <ul className="offer__offers-list">
                         <ul className="offer__cakes-list">
-                            <li className="offer__cake offer__cake--title">Torty</li>
-                            <li className="offer__cake offer__cake--normal">zwykłe</li>
-                            <li className="offer__cake offer__cake--wedding">weselne</li>
-                            <li className="offer__cake offer__cake--thematic">tematyczne</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--title offer__specialCake--title">Torty</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--item offer__specialCake--normal">zwykłe</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--item offer__specialCake--wedding">weselne</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--item offer__specialCake--thematic">tematyczne</li>
+                            <span className="offer__barrier"></span>
                         </ul>
                         <ul className="offer__commonCakes-list">
-                            <li className="offer__commonCake offer__commonCake--title">Ciasta</li>
-                            <li className="offer__commonCake offer__commonCake--traditional">tradycyjne</li>
-                            <li className="offer__commonCake offer__commonCake--dessert">deserowe</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--title offer__commonCake  offer__commonCake--title">Ciasta</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--item offer__commonCake  offer__commonCake--traditional">tradycyjne</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--item offer__commonCake  offer__commonCake--dessert">deserowe</li>
+                            <span className="offer__barrier"></span>
                         </ul>
                         <ul className="offer__cupCakes-list">
-                            <li className="offer__cupCake offer__cupCake--title">Babeczki</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--title offer__cupCake  offer__cupCake--title">Babeczki</li>
+                            <span className="offer__barrier"></span>
                         </ul>
                         <ul className="offer__occasional-list">
-                            <li className="offer__occasional offer__occasional--title">Wyroby okazjonalne</li>
+                            <li className="offer__cake offer__cake--icon offer__cake--title offer__occasional  offer__occasional--title">Wyroby okazjonalne</li>
                         </ul>
                     </ul>
                 </div>
+            </div>
+            {/* <OfferWave className="offer__wave"/> */}
+            <div className="offer__wave">
+                <img src={offerWave} alt="" className="offer__wave-img"/>
             </div>
         </div>
      );
