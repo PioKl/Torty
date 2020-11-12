1179201b00399837f5bc476d78e7b7239f2bbc78
PioKl <kosiu342@gmail.com>
2020-11-12 20:03:29 +0100

    Offer.js|Add custom arrows to slider

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -6,7 +6,30 @@ import { IsDesktopContext } from '../contexts/IsDesktopContext';
 import kucharz from "../images/kucharzMobile.png";
 import offerWave from "../images/offerWave.png";
 import Slider from "react-slick";
+/* import { ReactComponent as ArrowRight } from '../svg/arrowRight.svg'; */
 
+function SampleNextArrow(props) {
+    const { onClick } = props;
+    return (
+        /*<button type="button" onClick={onClick} className={`button button--text button--icon }`}>
+            <ArrowRight className="button" onClick={onClick} />
+        </button> */
+           /* <ArrowRight className="button" onClick={onClick} /> */
+           <button type="button" onClick={onClick} className={`offer-desktop__buttonArrow offer-desktop__buttonArrow--right`}>
+               {/* <ArrowRight className="str" /> */}
+           </button>
+           
+    )
+  }
+
+  function SamplePreviousArrow(props) {
+    const { onClick } = props;
+    return (
+           <button type="button" onClick={onClick} className={`offer-desktop__buttonArrow offer-desktop__buttonArrow--left`}>
+           </button>
+           
+    )
+  }
 
 const Offer = () => {
     const { width } = useContext(IsDesktopContext);
@@ -23,6 +46,8 @@ const Offer = () => {
         centerMode: true,
         centerPadding: "0px",
         speed: 500,
+        nextArrow: <SampleNextArrow/>,
+        prevArrow: <SamplePreviousArrow/>,
       };
 
     return ( 
