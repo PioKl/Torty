e2d59dddbe0dc32cba59ae6ae110e61c7b2611d7
PioKl <kosiu342@gmail.com>
2021-01-04 16:34:24 +0100

    Navigation.js|Add new href

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,6 +15,7 @@ const Navigation = () => {
                     <a href="#mainSite"><li className="desktop-itemList__item">Strona główna</li></a>
                     <a href="#offer"><li className="desktop-itemList__item">Oferta</li></a>
                     <a href="#gallery"><li className="desktop-itemList__item">Galeria</li></a>
+                    <a href="#about"><li className="desktop-itemList__item">O mnie</li></a>
                         <li className="desktop-itemList__item">Kontakt</li>
                     </ul>
                 </div>
