5b1cb361b73da9efd018af72ed38c3d2c5e453c1
PioKl <kosiu342@gmail.com>
2021-01-04 16:33:54 +0100

    About.js|Add IsDesktop context

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { IsDesktopContext } from '../contexts/IsDesktopContext';
 import '../style/About.scss';
 import person from '../images/personPhoto.png';
 import aboutWaveTop from '../images/aboutWaveTop.png';
 const About = () => {
+    const { width } = useContext(IsDesktopContext);
     return (
-        <div className="about">
+        <div id="about" className="about">
             <div className="about__section-title"></div>
             <div className="story">
                 <figure className="story__shape">
@@ -14,7 +16,7 @@ const About = () => {
                 <span className="story__barrier"></span>
                 <p className="story__description">Vestibulum suscipit, dolor eu pretium maximus, justo mi blandit turpis, a volutpat mauris nisl vitae purus. I, eleifend luctus tellus. Fusce luctus metus vitae dui ornare luctus. Curabitur condimentum bibendum lectus, sed efficitur mauris convallis nec. Morbi imperdiet purus ac ante molestie, ac ultrices urna gravida. </p>
             </div>
-            <img className="about__waveTop" src={aboutWaveTop} alt="" />
+            {width < 1280 ? <img className="about__waveTop" src={aboutWaveTop} alt="" /> : null}
         </div>
     );
 }
