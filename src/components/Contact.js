822cd684c11515ec0f051b346207bce934aee055
PioKl <kosiu342@gmail.com>
2021-01-05 19:21:54 +0100

    Contact.js|Add component Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { IsDesktopContext } from '../contexts/IsDesktopContext';
+import '../style/Contact.scss';
+import aboutContactWave from '../images/aboutContactWave.png'
+const Contact = () => {
+    const { width } = useContext(IsDesktopContext);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div id="contact" className="contact">
+            {width < 1280 ? <img className="contact__waveTop" src={aboutContactWave} alt="" /> : null}
+            <div className="contact__section-title"></div>
+            <form onSubmit={handleSubmit} className="form-contact" action="">
+                <h1 className="form-contact__title">Wyślij mi wiadomość</h1>
+                <label className="form-contact__item form-contact__item--name" htmlFor="name">Imię
+                    <input className="form-contact__input form-contact__input--name" type="text" id="name" name="name" required />
+                </label>
+                <label className="form-contact__item form-contact__item--email" htmlFor="email">e-mail
+                    <input className="form-contact__input form-contact__input--email" type="email" id="email" name="email" required />
+                </label>
+                <label className="form-contact__item form-contact__item--number" htmlFor="phone">numer telefonu
+                    <input className="form-contact__input form-contact__input--number" type="tel" id="phone" name="phone" required/*  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" */ />
+                </label>
+                <label className="form-contact__message" htmlFor="message">wiadomość
+                    <textarea className="form-contact__information" id="message" name="message" />
+                </label>
+                <button className="form-contact__sendButton">Wyślij</button>
+            </form>
+        </div>
+    );
+}
+
+export default Contact;
