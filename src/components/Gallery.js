75f1b030f35270723cc394179313cff7d50e8604
PioKl <kosiu342@gmail.com>
2021-01-11 17:40:45 +0100

    Gallery.js|Removal of unnecessary comments

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import '../style/Gallery.scss';
 import { IsDesktopContext } from '../contexts/IsDesktopContext';
 import { BurgerMenuContext } from '../contexts/BurgerMenuContext';
 import Slider from "react-slick";
 import galeria1 from '../images/galeria1.png';
+import galeria2 from '../images/galeria2.png';
+import galeria3 from '../images/galeria3.png';
+import galeria4 from '../images/galeria4.png';
 
 function SampleNextArrow(props) {
     const { onClick } = props;
@@ -24,6 +27,8 @@ function SamplePreviousArrow(props) {
 }
 
 const Gallery = () => {
+    const gallery = [galeria1, galeria2, galeria3, galeria4];
+    const cakeName = ['Klasyczny Tort', 'Urodzinowy Tort', 'Okazjonalny Tort', 'Weselny Tort']
     const { width } = useContext(IsDesktopContext);
     const { burgerMenu } = useContext(BurgerMenuContext);
     const settings = {
@@ -34,51 +39,52 @@ const Gallery = () => {
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePreviousArrow />,
     };
+    const [imageCounter, setImageCounter] = useState(0)
+
+    const handlePreviousImage = () => {
+        if (imageCounter <= 0) {
+            setImageCounter(gallery.length - 1);
+        }
+        else if (imageCounter >= 0) {
+            setImageCounter(imageCounter - 1);
+        }
+
+    }
+    const handleNextImage = () => {
+        setImageCounter(imageCounter + 1);
+        if (imageCounter === gallery.length - 1) {
+            setImageCounter(0);
+        }
+    }
     return (
         <>
             {width >= 1280 ?
                 <div id="gallery" className="gallery-desktop">
                     <div className="gallery-desktop__section-title"></div>
-                    {/* <div className="gallery-desktop__images-container"> */}
                     <Slider className="gallery-desktop__images-container" {...settings}>
-                        <div className="gallery-desktop__img-container">
-                            <img className="gallery-desktop__img" src={galeria1} alt="" />
-                            <div className="gallery-desktop__img-titleContainer">
-                                <h1 className="gallery-desktop__img-title">Klasyczny Tort</h1>
-                            </div>
-                        </div>
-                        <div className="gallery-desktop__img-container">
-                            <img className="gallery-desktop__img" src={galeria1} alt="" />
-                            <div className="gallery-desktop__img-titleContainer">
-                                <h1 className="gallery-desktop__img-title">Klasyczny Tort</h1>
-                            </div>
-                        </div>
-                        <div className="gallery-desktop__img-container">
-                            <img className="gallery-desktop__img" src={galeria1} alt="" />
-                            <div className="gallery-desktop__img-titleContainer">
-                                <h1 className="gallery-desktop__img-title">Klasyczny Tort</h1>
-                            </div>
-                        </div>
-                        <div className="gallery-desktop__img-container">
-                            <img className="gallery-desktop__img" src={galeria1} alt="" />
-                            <div className="gallery-desktop__img-titleContainer">
-                                <h1 className="gallery-desktop__img-title">Klasyczny Tort</h1>
-                            </div>
-                        </div>
+                        {gallery.map((image, index) => {
+                            return (
+                                <div key={index} className="gallery-desktop__img-container">
+                                    <img className="gallery-desktop__img" src={image} alt={cakeName[index]} />
+                                    <div className="gallery-desktop__img-titleContainer">
+                                        <h1 className="gallery-desktop__img-title">{cakeName[index]}</h1>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </Slider>
-                    {/* </div> */}
                 </div>
                 :
                 <div id="gallery" className={`gallery ${burgerMenu && 'grayScale'}`}>
                     <div className="gallery__section-title"></div>
                     <div className="gallery__images-container">
                         <div className="gallery__img-container">
-                            <button className="gallery__arrow gallery__arrow--left"></button>
-                            <img className="gallery__img" src={galeria1} alt="" />
-                            <button className="gallery__arrow gallery__arrow--right"></button>
+                            <button onClick={handlePreviousImage} className="gallery__arrow gallery__arrow--left"></button>
+                            <img className="gallery__img" src={gallery[imageCounter]} alt={cakeName[imageCounter]} />
+                            <button onClick={handleNextImage} className="gallery__arrow gallery__arrow--right"></button>
                         </div>
                         <div className="gallery__img-titleContainer">
-                            <h1 className="gallery__img-title">Klasyczny tort</h1>
+                            <h1 className="gallery__img-title">{cakeName[imageCounter]}</h1>
                         </div>
                     </div>
                 </div>
