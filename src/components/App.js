708888e99c13a76ea3a436ea9f49a2cdd1ff7e9b
PioKl <kosiu342@gmail.com>
2021-01-24 19:09:28 +0100

    Add gsap animations and new classes for animations

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
 import '../style/App.scss';
 
 import Navigation from './Navigation';
@@ -13,24 +14,42 @@ import IsDesktopContextProvider from '../contexts/IsDesktopContext';
 import BurgerMenuContextProvider from '../contexts/BurgerMenuContext';
 
 function App() {
+  useEffect(() => {
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+    tl.to(".cakeBox__left", { x: "-101%", duration: 1.5, delay: 0.5 });
+    tl.to(".cakeBox__right", { x: "101%", duration: 1.5, }, "-=1.5");
+    //autoAlpha jest za opacity, roznica jest taka, ze w opacity moge kliknac w jakis element, ktorego jeszcze sie nie pojawil, a autoAlpha to blokuje i moge kliknac dopiero jak bedzie widoczny
+    tl.fromTo(".navigation", { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 });
+    tl.fromTo(".hero-desktop__author", { autoAlpha: 0 }, { autoAlpha: 1, duration: 1.5 }, "-=1.3");
+  }, [])
   return (
-    <IsDesktopContextProvider>
-      <BurgerMenuContextProvider>
-        <div id="mainSite" className="App">
-          <header className="header">
-            <Navigation />
-          </header>
-          <main className="main">
-            <Hero />
-            <Offer />
-            <Gallery />
-            <About />
-            <Contact />
-          </main>
-          <Footer />
+    <>
+      <IsDesktopContextProvider>
+        <BurgerMenuContextProvider>
+          <div id="mainSite" className="App">
+            <header className="header">
+              <Navigation />
+            </header>
+            <main className="main">
+              <Hero />
+              <Offer />
+              <Gallery />
+              <About />
+              <Contact />
+            </main>
+            <Footer />
+          </div>
+        </BurgerMenuContextProvider>
+      </IsDesktopContextProvider>
+      <div className="cakeBox">
+        <div className="cakeBox__left">
         </div>
-      </BurgerMenuContextProvider>
-    </IsDesktopContextProvider>
+        <div className="cakeBox__right">
+
+        </div>
+      </div>
+    </>
   );
 }
 
