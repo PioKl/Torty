64f71f989e0749f3b9c327a97954609816390ba4
PioKl <kosiu342@gmail.com>
2021-02-12 16:41:51 +0100

    New spinner

--- src/components/App.js
+++ src/components/App.js
@@ -63,7 +63,20 @@ function App() {
         </div>
       </> :
         <>
-          <div className="loader"></div>
+          <div class="sk-circle">
+            <div class="sk-circle1 sk-child"></div>
+            <div class="sk-circle2 sk-child"></div>
+            <div class="sk-circle3 sk-child"></div>
+            <div class="sk-circle4 sk-child"></div>
+            <div class="sk-circle5 sk-child"></div>
+            <div class="sk-circle6 sk-child"></div>
+            <div class="sk-circle7 sk-child"></div>
+            <div class="sk-circle8 sk-child"></div>
+            <div class="sk-circle9 sk-child"></div>
+            <div class="sk-circle10 sk-child"></div>
+            <div class="sk-circle11 sk-child"></div>
+            <div class="sk-circle12 sk-child"></div>
+          </div>
           <div className="cakeBox cakeBoxShade">
             <div className="cakeBox__seal"></div>
             <div className="cakeBox__left">
